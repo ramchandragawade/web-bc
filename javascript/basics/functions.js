2eaafc2c4ad27bbc58cc9ffada256a9f79378b52
ramchandra gawade <raajgawade39@gmail.com>
2023-08-20 02:03:18 +0530

    try catch 2

--- javascript/basics/functions.js
+++ javascript/basics/functions.js
@@ -124,4 +124,13 @@ try {
 } catch {
     console.log('ERRORRRRR!!!!');
 }
-console.log('After')
+console.log('After');
+
+function yell (msg) {
+    try{
+        console.log(msg.toUpperCase().repeat(3));
+    } catch (e) {
+        console.log('Please pass a string');
+        console.log(e);
+    }
+}
