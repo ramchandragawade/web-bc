227679d3a699e8b0bd80e728c4d322168a06d278
ramchandra gawade <raajgawade39@gmail.com>
2023-08-20 01:27:25 +0530

    Higher order func

--- javascript/basics/functions.js
+++ javascript/basics/functions.js
@@ -17,3 +17,17 @@
 //     cryForHelp();
 // }
 // bankRoberry();
+
+// const add = function (x,y) {
+//     return x+y;
+// };
+
+function callTwice (func) {
+    func();
+    func();
+}
+function rollDice(){
+    const roll = Math.floor(Math.random()*6)+1
+    console.log(roll);
+}
+callTwice(rollDice);
