18e984cb69ef22ac519ff0a9d9b0315bf2a659ba
ramchandra gawade <raajgawade39@gmail.com>
2023-08-20 01:41:06 +0530

    methods

--- javascript/basics/functions.js
+++ javascript/basics/functions.js
@@ -33,36 +33,55 @@
 // callTwice(rollDice);
 
 
-function makeMysteryFunc(){
-    const rand = Math.random();
-    if(rand > 0.5){
-        return function(){
-            console.log('This is the first function');
-        }
-    } else {
-        return function(){
-            console.log('Second func');
-        }
+// function makeMysteryFunc(){
+//     const rand = Math.random();
+//     if(rand > 0.5){
+//         return function(){
+//             console.log('This is the first function');
+//         }
+//     } else {
+//         return function(){
+//             console.log('Second func');
+//         }
+//     }
+// }
+
+// const mystery = makeMysteryFunc();
+// mystery();
+
+// function isBetween(num) {
+//     return num>=10 && num<=50;
+// }
+
+// function makeBetweenFunc(min,max) {
+//     return function(num){
+//         return num>=min && num<=max; 
+//     }
+// }
+
+// const isChild = makeBetweenFunc(0,10);
+// const isAdult = makeBetweenFunc(18,63);
+// const isSenior = makeBetweenFunc(63,120);
+
+// console.log('is 5 a child:'+isChild(5))
+// console.log('is 23 a isAdult:'+isAdult(23))
+// console.log('is 60 a senior:'+isSenior(60))
+
+
+//Method: function defined as a property is method. All methods are function but not vice versa;
+const myMath = {
+    PI: 3.14,
+    square: function (num){
+        return num*num;
+    },
+    cube: function (num){
+        return num*num*num;
+    },
+    sub(num1,num2){
+        return num1-num2;
     }
 }
 
-const mystery = makeMysteryFunc();
-mystery();
-
-function isBetween(num) {
-    return num>=10 && num<=50;
-}
-
-function makeBetweenFunc(min,max) {
-    return function(num){
-        return num>=min && num<=max; 
-    }
-}
-
-const isChild = makeBetweenFunc(0,10);
-const isAdult = makeBetweenFunc(18,63);
-const isSenior = makeBetweenFunc(63,120);
-
-console.log('is 5 a child:'+isChild(5))
-console.log('is 23 a isAdult:'+isAdult(23))
-console.log('is 60 a senior:'+isSenior(60))
+console.log(myMath.cube(2));
+console.log(myMath.square(3));
+console.log(myMath.sub(11,5));
