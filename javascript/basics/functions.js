90b64e8049a7980e57c5abcf93cead63892e4eef
ramchandra gawade <raajgawade39@gmail.com>
2023-08-20 01:35:26 +0530

    returning funcs

--- javascript/basics/functions.js
+++ javascript/basics/functions.js
@@ -22,12 +22,47 @@
 //     return x+y;
 // };
 
-function callTwice (func) {
-    func();
-    func();
+// function callTwice (func) {
+//     func();
+//     func();
+// }
+// function rollDice(){
+//     const roll = Math.floor(Math.random()*6)+1
+//     console.log(roll);
+// }
+// callTwice(rollDice);
+
+
+function makeMysteryFunc(){
+    const rand = Math.random();
+    if(rand > 0.5){
+        return function(){
+            console.log('This is the first function');
+        }
+    } else {
+        return function(){
+            console.log('Second func');
+        }
+    }
 }
-function rollDice(){
-    const roll = Math.floor(Math.random()*6)+1
-    console.log(roll);
+
+const mystery = makeMysteryFunc();
+mystery();
+
+function isBetween(num) {
+    return num>=10 && num<=50;
 }
-callTwice(rollDice);
+
+function makeBetweenFunc(min,max) {
+    return function(num){
+        return num>=min && num<=max; 
+    }
+}
+
+const isChild = makeBetweenFunc(0,10);
+const isAdult = makeBetweenFunc(18,63);
+const isSenior = makeBetweenFunc(63,120);
+
+console.log('is 5 a child:'+isChild(5))
+console.log('is 23 a isAdult:'+isAdult(23))
+console.log('is 60 a senior:'+isSenior(60))
