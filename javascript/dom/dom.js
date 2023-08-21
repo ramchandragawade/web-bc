fe9caac1beadf70ba5ca1866797222d230301b49
ramchandra gawade <raajgawade39@gmail.com>
2023-08-22 01:43:35 +0530

    dom selecting

---
+++ javascript/dom/dom.js
@@ -0,0 +1,36 @@
+// console.dir(document)
+
+// document.all[10].innerText = 'SSSS'
+
+// ban = document.getElementById('banner')
+// console.dir(ban)
+
+// toc = document.getElementById('toc')
+// console.dir(toc)
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let item of allImages ) {
+//     console.log(item.src);
+//     // item.src ='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+// const byClass = document.getElementsByClassName('square');
+// for (let item of byClass ) {
+//     console.log(item.src);
+//     // item.src ='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+// }
+
+// //shows only first result
+// document.querySelector('p');
+// document.querySelector('a[title="Java"]');
+
+// //shows all
+// document.querySelectorAll('p');
+// document.querySelectorAll('a');
+// document.querySelectorAll('p a');
+
+// const links = document.querySelectorAll('p a');
+// for (let item of byClass ) {
+//     console.log(item.href);
+// }
