fef23ab4410ad33081e12ef89a00b43c4c91e856
ramchandra gawade <raajgawade39@gmail.com>
2023-08-24 01:36:56 +0530

    prevent default exercise

--- javascript/dom/dom_events/formEvents/app.js
+++ javascript/dom/dom_events/formEvents/app.js
@@ -1,5 +1,12 @@
 const form = document.querySelector('#shelterForm');
+const input = document.querySelector('#catName');
+const ul = document.querySelector('#cats');
 form.addEventListener('submit', function(e){
-    console.log('Submitted!!!')
     e.preventDefault();
+    console.log(input.value);
+    const catName = input.value;
+    const newLI = document.createElement('li');
+    newLI.innerText = catName;
+    ul.append(newLI);
+    input.value = '';
 });
