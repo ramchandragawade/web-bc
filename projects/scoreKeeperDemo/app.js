44388dddc37603329613b08101682171c5d3d8dc
Ramchandra Gawade <ramchandra.gawade@northgateps.com>
2023-08-25 00:52:34 +0530

    score keeper base functionality

---
+++ projects/scoreKeeperDemo/app.js
@@ -0,0 +1,50 @@
+const p1btn = document.querySelector('#p1btn');
+const p2btn = document.querySelector('#p2btn');
+const resetBtn = document.querySelector('#reset');
+const p1display = document.querySelector('#p1display');
+const p2display = document.querySelector('#p2display');
+const playTill = document.querySelector('#playTill');
+let winningScore = 5;
+let isGameOver = false;
+let p1score = 0;
+let p2score = 0;
+p1btn.addEventListener('click', function(){
+    if(!isGameOver){
+        p1score += 1;
+        if (p1score === winningScore) {
+            isGameOver = true;
+            p1display.classList.add('winner');
+            p2display.classList.add('loser');
+        }
+        p1display.textContent = p1score;
+    }
+});
+
+p2btn.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2score += 1;
+        if (p2score === winningScore) {
+            isGameOver = true;
+            p2display.classList.add('winner');
+            p1display.classList.add('loser');
+        }
+        p2display.textContent = p2score;
+    }
+});
+
+resetBtn.addEventListener('click',resetAll);
+
+playTill.addEventListener('change',function(){
+    winningScore = parseInt(this.value);
+    resetAll();
+});
+
+function resetAll () {
+    p1score = 0;
+    p2score = 0;
+    isGameOver = false;
+    p1display.textContent = 0;
+    p2display.textContent = 0;
+    p1display.classList.remove('winner', 'loser');
+    p2display.classList.remove('winner', 'loser');
+}
