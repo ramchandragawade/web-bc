6f7a46ea9e15aad48a606989536b3b1ff9673b62
Ramchandra Gawade <ramchandra.gawade@northgateps.com>
2023-08-28 23:48:52 +0530

    fakeReq with promises

--- javascript/asyncJs/exercise/promiseBasic/app.js
+++ javascript/asyncJs/exercise/promiseBasic/app.js
@@ -24,24 +24,49 @@ const fakeRequestPromise = (url) => {
 }
 
 //Callback fakereq, cbhell
-fakeRequestCallback('books.com', function(response){
-    console.log('IT WORKED!!!!')    
-    console.log(response);
-    fakeRequestCallback('books.com/page2', function (response) {
-        console.log('IT WORKED AGAIN!!!!')
-        console.log(response);
-        fakeRequestCallback('books.com/page3', function (response) {
-            console.log('3rd request worked!!!!')
-            console.log(response);
-        }, function (error) {
-            console.log('ERROR on 3rd page!!!!');
-            console.log(error);
-        });
-    }, function (error) {
-        console.log('ERROR on 2nd page!!!!');
-        console.log(error);
-    });
-},function(error){
-    console.log('ERROR!!!!');
-    console.log(error);
+// fakeRequestCallback('books.com', function(response){
+//     console.log('IT WORKED!!!!')    
+//     console.log(response);
+//     fakeRequestCallback('books.com/page2', function (response) {
+//         console.log('IT WORKED AGAIN!!!!')
+//         console.log(response);
+//         fakeRequestCallback('books.com/page3', function (response) {
+//             console.log('3rd request worked!!!!')
+//             console.log(response);
+//         }, function (error) {
+//             console.log('ERROR on 3rd page!!!!');
+//             console.log(error);
+//         });
+//     }, function (error) {
+//         console.log('ERROR on 2nd page!!!!');
+//         console.log(error);
+//     });
+// },function(error){
+//     console.log('ERROR!!!!');
+//     console.log(error);
+// });
+
+
+//fakeReq with promise
+
+fakeRequestPromise('books').then(function(data){
+    console.log('It worked P1');
+    console.log(data);
+    fakeRequestPromise('books/p2').then(function (data) {
+        console.log('It worked P2');
+        console.log(data);
+        fakeRequestPromise('books/p3').then(function (data) {
+            console.log('It worked P3');
+            console.log(data);
+        }).catch(function (e) {
+            console.log('P3 Failed');
+            console.log(e);
+        })
+    }).catch(function (e) {
+        console.log('P2 Failed');
+        console.log(e);
+    })
+}).catch(function(e){
+    console.log('P1 Failed');
+    console.log(e);
 });
