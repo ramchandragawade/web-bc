70f9a392c8bed6f9d51ea8b8d13db9b32c7a629f
Ramchandra Gawade <ramchandra.gawade@northgateps.com>
2023-08-29 00:38:51 +0530

    handle rejection/errors

--- javascript/asyncJs/exercise/asyncFn/app.js
+++ javascript/asyncJs/exercise/asyncFn/app.js
@@ -68,14 +68,42 @@ function delayedColorChange(newColor, delay) {
 
 //ASync await
 
-async function rainbow(){
-    await delayedColorChange('violet', 1000);
-    await delayedColorChange('indigo', 1000);
-    await delayedColorChange('blue', 1000);
-    await delayedColorChange('green', 1000);
-    await delayedColorChange('yellow', 1000);
-    await delayedColorChange('orange', 1000);
-    await delayedColorChange('red', 1000);
-    return 'All Done'
+// async function rainbow(){
+//     await delayedColorChange('violet', 1000);
+//     await delayedColorChange('indigo', 1000);
+//     await delayedColorChange('blue', 1000);
+//     await delayedColorChange('green', 1000);
+//     await delayedColorChange('yellow', 1000);
+//     await delayedColorChange('orange', 1000);
+//     await delayedColorChange('red', 1000);
+//     return 'All Done'
+// }
+// rainbow().then((data)=>console.log(data));
+
+
+//Handling rejection/errors
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 3000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
 }
-rainbow().then((data)=>console.log(data));
+
+async function makeTwoReq(){
+    try {
+
+        let data1 = await fakeRequestPromise('/page1');
+        let data2 = await fakeRequestPromise('/page2');
+        console.log(data1);
+        console.log(data2);
+    } catch (error) {
+        console.log('ERRORRRR:',error);
+    }
+}
+makeTwoReq();
