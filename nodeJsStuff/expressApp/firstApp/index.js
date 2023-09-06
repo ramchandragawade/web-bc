abbf7b753264dfa6efafe8fb6bd2f141b7c97caf
ramchandra gawade <raajgawade39@gmail.com>
2023-09-06 22:11:40 +0530

    express query

--- nodeJsStuff/expressApp/firstApp/index.js
+++ nodeJsStuff/expressApp/firstApp/index.js
@@ -26,10 +26,30 @@ app.get('/dogs',(req,res)=>{
     res.send('Wooofffff');
 })
 
+app.get('/r/:subreddit/:sortBy',(req,res)=>{
+    // console.log(req.params);
+    const {subreddit:subReddit,sortBy} = req.params;
+    res.send(`This is a Subreddit ${subReddit} sorted by ${sortBy}`);
+})
+
+app.get('/search',(req,res)=>{
+    // console.log(req.query);
+    const {q,color} = req.query;
+    if(!q){
+        res.send('Nothing found nothing send');
+    } else {
+        let msg =q;
+        if(color){
+            msg += ' and also search for color:' +color;
+        }
+        res.send('Search results for : '+msg);
+    }
+})
+
+//Add this in the end otherwise it will overwrite
 app.get('*',(req,res)=>{
     res.send('Route does not exist');
 })
-
 
 app.listen(8080,()=>{
     console.log('Listening on port 8080');
