651bed6dd18a23aaca6cd732b52cc7e8145a4524
ramchandra gawade <raajgawade39@gmail.com>
2023-09-06 21:52:43 +0530

    express routes

--- nodeJsStuff/expressApp/firstApp/index.js
+++ nodeJsStuff/expressApp/firstApp/index.js
@@ -2,13 +2,34 @@ const express = require('express');
 const app = express();
 // console.log(app);
 
-app.use((req, res)=>{
-    console.log('Yo new request');
-    // console.dir(req);
-    // res.send('Hi, got the request');
-    // res.send({color: 'red'});
-    res.send('<h1>Hi, got the request</h1>');
+// app.use((req, res)=>{
+//     console.log('Yo new request');
+//     // console.dir(req);
+//     // res.send('Hi, got the request');
+//     // res.send({color: 'red'});
+//     res.send('<h1>Hi, got the request</h1>');
+// })
+
+app.get('/',(req,res)=>{
+    res.send('Home Page');
 })
+
+app.get('/cats',(req,res)=>{
+    res.send('Meowwwrrr');
+})
+
+app.post('/cats',(req,res)=>{
+    res.send('Meowwwrrr but from POST');
+})
+
+app.get('/dogs',(req,res)=>{
+    res.send('Wooofffff');
+})
+
+app.get('*',(req,res)=>{
+    res.send('Route does not exist');
+})
+
 
 app.listen(8080,()=>{
     console.log('Listening on port 8080');
