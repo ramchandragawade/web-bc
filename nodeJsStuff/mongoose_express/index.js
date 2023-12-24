b10e176805d81f00f134a4ddd466f4baf526b1cd
ramchandra gawade <raajgawade39@gmail.com>
2023-12-25 04:02:59 +0530

    show & edit prod

--- nodeJsStuff/mongoose_express/index.js
+++ nodeJsStuff/mongoose_express/index.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require('path');
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine','ejs');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
 const mongoose = require('mongoose');
 const Product = require('./models/product');
+const { log } = require('console');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmMarket')
 .then(()=>{
@@ -38,6 +41,19 @@ app.get('/products/:id',async (req,res)=>{
     // console.log(prod);
     // res.send('Details page');
     res.render('products/show',{prod});
+});
+
+app.get('/products/:id/edit',async (req,res)=>{
+    const { id } = req.params;
+    const prod = await Product.findById(id);
+    res.render('products/edit',{prod});
+});
+
+app.put('/products/:id',async (req,res)=>{
+    const { id } = req.params;
+    const editedProd = await Product.findByIdAndUpdate(id,req.body,{runValidators: true, new: true});
+    console.log(editedProd);
+    res.redirect(`/products/${editedProd._id}`);
 });
 
 app.listen(3000,()=>{
