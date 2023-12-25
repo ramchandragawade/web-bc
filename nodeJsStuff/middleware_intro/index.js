b9efa3160f0195d689490a823ff42a5c66c34bb4
ramchandra gawade <raajgawade39@gmail.com>
2023-12-25 20:13:39 +0530

    middle ware intro

---
+++ nodeJsStuff/middleware_intro/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use((req,res,next)=>{
+    console.log('In 1 use...');
+    next();
+    console.log('In 1 use but after...');
+});
+
+app.use((req,res,next)=>{
+    console.log('In 2 use...');
+    return next();
+    console.log('In 2 use but after...');
+});
+
+app.get('/',(req,res)=>{
+    res.send('Home');
+});
+
+app.get('/dogs',(req,res)=>{
+    res.send('Dogsgsgsgsg');
+})
+
+app.listen(3000,()=>{
+    console.log('App is running on 3000');
+});
