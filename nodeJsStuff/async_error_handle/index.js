096c2af523a555ef9b843b61ac4070d6dc69a1b9
ramchandra gawade <raajgawade39@gmail.com>
2023-12-26 17:45:50 +0530

    wrapper func for error handling

--- nodeJsStuff/async_error_handle/index.js
+++ nodeJsStuff/async_error_handle/index.js
@@ -22,82 +22,67 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmMarket2')
     console.log('MONGO Failure!!!!'+err);
 });
 
-app.get("/products", async (req, res, next) => {
-    try {
-        let { category } = req.query;
-        if (category) {
-          const allProd = await Product.find({ category });
-          res.render("products/index", { allProd, category });
-        } else {
-          const allProd = await Product.find({});
-          res.render("products/index", { allProd, category:'All' });
-        }      
-    } catch(e) {
-        next(e);
+function wrapAsync(fn,req,res,next){
+    return function(req,res,next) {
+        console.log('In wrapper',req.params);
+        fn(req,res,next).catch(e=>next(e));
     }
-});
+}
+
+
+app.get("/products",wrapAsync( async (req, res, next) => {
+    let { category } = req.query;
+    if (category) {
+      const allProd = await Product.find({ category });
+      res.render("products/index", { allProd, category });
+    } else {
+      const allProd = await Product.find({});
+      res.render("products/index", { allProd, category:'All' });
+    }
+}));
 
 app.get('/products/new',(req,res)=>{
     // throw new AppError('Not allowed',401);
     res.render('products/new', {categories});
 });
 
-app.post('/products',async (req,res,next)=>{
-    try {
-        const newProd = new Product(req.body);
-        await newProd.save();
-        // console.log(newProd);
-        res.redirect('/products');
-    } catch(e) {
-        next(e);
-    }
-});
+app.post('/products',wrapAsync(async (req,res,next)=>{
+    const newProd = new Product(req.body);
+    await newProd.save();
+    res.redirect('/products');
+}));
 
-app.get('/products/:id',async (req,res,next)=>{
-    try {
-        const { id } = req.params;
-        const prod = await Product.findById(id);
-        // console.log(prod);
-        // res.send('Details page');
-        // throw new AppError('Not allowed',401);
-        if(!prod) {
-            throw new AppError('Product not found', 404);
-        }
-        res.render('products/show',{prod});    
-    } catch(e) {
-        next(e);
-    }
-});
 
-app.get('/products/:id/edit',async (req,res,next)=>{
-    try {
-        const { id } = req.params;
-        const prod = await Product.findById(id);
-        if(!prod) {
-            throw new AppError('Product not found', 404);
-        }
-        res.render('products/edit',{prod,categories});    
-    } catch(e) {
-        next(e);
+app.get('/products/:id', wrapAsync(async (req,res,next)=>{
+    const { id } = req.params;
+    const prod = await Product.findById(id);
+    if(!prod) {
+        throw new AppError('Product not found', 404);
     }
-});
+    res.render('products/show',{prod});
+}));
 
-app.put('/products/:id',async (req,res,next)=>{
-    try {
-        const { id } = req.params;
-        const editedProd = await Product.findByIdAndUpdate(id,req.body,{runValidators: true, new: true});
-        res.redirect(`/products/${editedProd._id}`);    
-    } catch(e){
-        next(e);
+app.get('/products/:id/edit',wrapAsync(async (req,res,next)=>{
+    const { id } = req.params;
+    const prod = await Product.findById(id);
+    if(!prod) {
+        throw new AppError('Product not found', 404);
     }
-});
+    res.render('products/edit',{prod,categories}); 
+}));
 
-app.delete('/products/:id',async (req,res)=>{
+app.put('/products/:id',wrapAsync(async (req,res,next)=>{
+    const { id } = req.params;
+    const editedProd = await Product.findByIdAndUpdate(id,req.body,{runValidators: true, new: true});
+    res.redirect(`/products/${editedProd._id}`);   
+}));
+
+app.delete('/products/:id',wrapAsync(async (req,res,next)=>{
     const { id } = req.params;
     const prod = await Product.findByIdAndDelete(id);
     console.log(prod);
     res.redirect('/products');
-});
+}));
 
 app.use((err,req,res,next)=>{
     const {status=500,message='Something went wrong'} = err;
