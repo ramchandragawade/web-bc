56ad96f5be57acd1c8dc520164dfd4ae68196366
ramchandragawade <raajgawade39@gmail.com>
2023-12-28 22:31:24 +0530

    login middleware

--- moreNode/demoAuth/index.js
+++ moreNode/demoAuth/index.js
@@ -15,7 +15,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
 
 app.set('view engine','ejs');
 app.set('views','views');
-
+const requireLogin = (req,res,next)=>{
+    if(!req.session.user_id){
+        return res.redirect('/login');
+    }
+    next();
+}
 app.use(express.urlencoded({extended:true}));
 app.use(session({
     secret:'IAMTHEPROBLEM',
@@ -65,14 +70,15 @@ app.post('/register', async(req,res)=>{
 app.post('/logout', async(req,res)=>{
     // req.session.user_id = null;
     req.session.destroy();
-    res.redirect('/');
+    res.redirect('/login');
 });
 
-app.get('/secret',(req,res)=>{
-    if(!req.session.user_id){
-        return res.redirect('/login');
-    }
+app.get('/secret',requireLogin, (req,res)=>{
     res.render('secret');
+});
+
+app.get('/topsecret',requireLogin, (req,res)=>{
+    res.send('bwaaahahahahhahaha!!!');
 });
 
 app.listen(3030,()=>{
