81faf2ff7b6abf90669303d7ffdcc550a61577c1
ramchandragawade <raajgawade39@gmail.com>
2023-12-28 18:04:09 +0530

    created campgroud router

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -21,22 +21,13 @@ db.once('open', ()=>{
 const app = express();
 const path = require('path');
 
+const campgroundRoutes = require('./routes/campground');
+
 app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-
-const validateCampground = (req,res,next)=>{
-    const {error} = campgroundSchema.validate(req.body);
-    if(error) {
-        console.log(error);
-        const msg = error.details.map(el=>el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
 
 const validateReview = (req,res,next)=>{
     const {error} = reviewSchema.validate(req.body);
@@ -53,50 +44,7 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-// Campgroung index route
-app.get('/campgrounds', async(req,res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-});
-
-// New campground form route
-app.get('/campgrounds/new', (req,res)=>{
-    res.render('campgrounds/new');
-});
-
-// Add New campground form submission route(POST)
-app.post('/campgrounds',validateCampground, catchAsync(async(req,res,next)=>{
-    const newCamp = new Campground(req.body.campground);
-    await newCamp.save();
-    res.redirect('/campgrounds');
-}));
-
-// Show selected campground route
-app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
-    const {id} = req.params;
-    const camp = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show', {camp});
-}));
-
-// Update edited campground route
-app.put('/campgrounds/:id',validateCampground, catchAsync(async(req,res)=>{
-    const {id} = req.params;
-    const camp = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-    res.redirect(`/campgrounds/${camp._id}`);
-}));
-
-// delete campground route
-app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
-    const {id} = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect(`/campgrounds`);
-}));
-
-// Edit campground form route
-app.get('/campgrounds/:id/edit',catchAsync(async(req,res)=>{
-    const camp = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {camp});
-}));
+app.use('/campgrounds',campgroundRoutes);
 
 // Add campground review form route(POST)
 app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async(req,res)=>{
---
+++ YelpCamp/routes/campground.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
+const Campground = require('../models/campground');
+const {campgroundSchema} = require('../validationSchemas');
+
+
+
+const validateCampground = (req,res,next)=>{
+    const {error} = campgroundSchema.validate(req.body);
+    if(error) {
+        console.log(error);
+        const msg = error.details.map(el=>el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+// Campgroung index route
+router.get('/', async(req,res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+});
+
+// New campground form route
+router.get('/new', (req,res)=>{
+    res.render('campgrounds/new');
+});
+
+// Add New campground form submission route(POST)
+router.post('/',validateCampground, catchAsync(async(req,res,next)=>{
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect('/campgrounds');
+}));
+
+// Show selected campground route
+router.get('/:id', catchAsync(async(req,res)=>{
+    const {id} = req.params;
+    const camp = await Campground.findById(id).populate('reviews');
+    res.render('campgrounds/show', {camp});
+}));
+
+// Update edited campground route
+router.put('/:id',validateCampground, catchAsync(async(req,res)=>{
+    const {id} = req.params;
+    const camp = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${camp._id}`);
+}));
+
+// delete campground route
+router.delete('/:id',catchAsync(async(req,res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect(`/campgrounds`);
+}));
+
+// Edit campground form route
+router.get('/:id/edit',catchAsync(async(req,res)=>{
+    const camp = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {camp});
+}));
+
+module.exports = router;
