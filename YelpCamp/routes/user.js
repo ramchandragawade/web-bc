f527492fcf2016731f0ff83c43f391cfcaac9889
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 00:41:51 +0530

    login after register

--- YelpCamp/routes/user.js
+++ YelpCamp/routes/user.js
@@ -8,14 +8,16 @@ router.get('/register',(req,res)=>{
     res.render('users/register');
 });
 
-router.post('/register', catchAsync(async(req,res)=>{
+router.post('/register', catchAsync(async(req,res,next)=>{
     try {
         const {username,password,email} = req.body;
         const user = new User({email,username});
         const registeredUser = await User.register(user,password);
-        console.log(registeredUser);
-        req.flash('success','Welcome to YelpCamp! '+ username);
-        res.redirect('/campgrounds');    
+        req.login(registeredUser,err=>{
+            if(err) return next(err)
+            req.flash('success','Welcome to YelpCamp! '+ username);
+            res.redirect('/campgrounds');
+        });
     } catch(e) {
         req.flash('error', e.message);
         res.redirect('/register');
