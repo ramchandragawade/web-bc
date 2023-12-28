038fcc3e8aff1012618b5bf4241476ef8717e35a
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 02:42:15 +0530

    review auth to add review

--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router({mergeParams:true});
-const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 
 // Add campground review form route(POST)
-router.post('/',validateReview, catchAsync(async(req,res)=>{
+router.post('/', isLoggedIn, validateReview, catchAsync(async(req,res)=>{
     const { id } = req.params;
     const camp = await Campground.findById(id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     camp.reviews.push(review);
     await review.save();
     await camp.save();
