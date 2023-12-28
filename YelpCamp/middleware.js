874e80bf859ffaf0f048e23a5bad966ff34f27c8
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 02:27:53 +0530

    refactor

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema } = require('./validationSchemas');
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -10,6 +14,29 @@ module.exports.isLoggedIn = (req,res,next)=>{
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+// Campground Validator middleware
+module.exports.validateCampground = (req,res,next)=>{
+    const {error} = campgroundSchema.validate(req.body);
+    if(error) {
+        console.log(error);
+        const msg = error.details.map(el=>el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+// Check if user is author
+module.exports.isAuthor = async(req,res,next)=>{
+    const {id} = req.params;
+    const camp = await Campground.findById(id);
+    if(!camp.author.equals(req.user._id)){
+        req.flash('error',`You don't have permission to update.`);
+        return res.redirect(`/campgrounds/${id}`);        
     }
     next();
 }
--- YelpCamp/routes/campground.js
+++ YelpCamp/routes/campground.js
@@ -1,22 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../validationSchemas');
-const { isLoggedIn } = require('../middleware');
-
-// Campground Validator middleware
-const validateCampground = (req,res,next)=>{
-    const {error} = campgroundSchema.validate(req.body);
-    if(error) {
-        console.log(error);
-        const msg = error.details.map(el=>el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 // Campgroung index route
 router.get('/', async(req,res)=>{
@@ -50,41 +36,28 @@ router.get('/:id', catchAsync(async(req,res)=>{
 }));
 
 // Update edited campground route
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async(req,res)=>{
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async(req,res)=>{
     const {id} = req.params;
-    const camp = await Campground.findById(id);
-    if(!camp.author.equals(req.user._id)){
-        req.flash('error',`You don't have permission to update.`);
-        return res.redirect(`/campgrounds/${id}`);        
-    }
     const updatedCamp = await Campground.findByIdAndUpdate(id,{...req.body.campground},{new:true});
     req.flash('success',`Successfully updated ${updatedCamp.title}!`);
     res.redirect(`/campgrounds/${updatedCamp._id}`);
 }));
 
 // delete campground route
-router.delete('/:id',isLoggedIn,catchAsync(async(req,res)=>{
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async(req,res)=>{
     const {id} = req.params;
-    if(!camp.author.equals(req.user._id)){
-        req.flash('error',`You don't have permission to delete.`);
-        return res.redirect(`/campgrounds/${id}`);        
-    }
     await Campground.findByIdAndDelete(id);
     req.flash('success',`Successfully deleted the campground!`);
     res.redirect(`/campgrounds`);
 }));
 
 // Edit campground form route
-router.get('/:id/edit', isLoggedIn, catchAsync(async(req,res)=>{
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async(req,res)=>{
     const { id } = req.params;
     const camp = await Campground.findById(id);
     if(!camp){
         req.flash('error','Campground not found!');
         return res.redirect('/campgrounds');
-    }
-    if(!camp.author.equals(req.user._id)){
-        req.flash('error',`You don't have permission to update.`);
-        return res.redirect(`/campgrounds/${id}`);        
     }
     res.render('campgrounds/edit', {camp});
 }));
