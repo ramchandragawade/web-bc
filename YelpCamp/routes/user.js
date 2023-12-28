7a732a70bd5d94411e01175dd4d4659fada52647
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 01:38:55 +0530

    return to setup

--- YelpCamp/routes/user.js
+++ YelpCamp/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register',(req,res)=>{
     res.render('users/register');
@@ -28,9 +29,12 @@ router.get('/login',(req,res)=>{
     res.render('users/login');
 });
 
-router.post('/login', passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}), async(req,res)=>{
+router.post('/login', storeReturnTo, passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}), async(req,res)=>{
     req.flash('success',`Welcome back ${req.body.username}!!!`);
-    res.redirect('/campgrounds'); 
+    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    delete res.locals.returnTo;
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 });
 
 router.get('/logout',(req,res)=>{
