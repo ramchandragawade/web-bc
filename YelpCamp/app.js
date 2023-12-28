d24f0ef639836e057ce86634ac5633dec2cf7451
ramchandragawade <raajgawade39@gmail.com>
2023-12-28 23:14:34 +0530

    early passport setup

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
+const path = require('path');
+const campgroundRoutes = require('./routes/campground');
+const reviewRoutes = require('./routes/reviews');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const db = mongoose.connection;
@@ -12,10 +18,7 @@ db.once('open', ()=>{
 });
 
 const app = express();
-const path = require('path');
 
-const campgroundRoutes = require('./routes/campground');
-const reviewRoutes = require('./routes/reviews');
 app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -36,12 +39,19 @@ const sessionCfg = {
 }
 app.use(session(sessionCfg));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
+
 // Home route
 app.get('/', (req, res) => {
     res.render('home');
