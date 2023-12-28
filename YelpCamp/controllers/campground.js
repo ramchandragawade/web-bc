d766baaf36bdb701f3df4264d516d662880a606b
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 03:31:33 +0530

    added comments

--- YelpCamp/controllers/campground.js
+++ YelpCamp/controllers/campground.js
@@ -1,14 +1,17 @@
 const Campground = require('../models/campground');
 module.exports = {
+    // Campgroung index route
     index : async(req,res)=>{
         const campgrounds = await Campground.find({});
         res.render('campgrounds/index', {campgrounds});
     },
-    
+
+    // New campground form route
     renderNewForm: (req,res)=>{
         res.render('campgrounds/new');
     },
-    
+
+    // Add New campground form submission route(POST)
     createCamp: async(req,res,next)=>{
         const newCamp = new Campground(req.body.campground);
         newCamp.author = req.user._id;
@@ -16,7 +19,8 @@ module.exports = {
         req.flash('success','Successfully added a new campground!');
         res.redirect('/campgrounds/'+newCamp._id);
     },
-    
+
+    // Show selected campground route
     showCamp: async(req,res)=>{
         const {id} = req.params;
         const camp = await Campground.findById(id).populate({
@@ -31,7 +35,8 @@ module.exports = {
         }
         res.render('campgrounds/show', {camp});
     },
-    
+
+    // Edit campground form route
     renderEditForm: async(req,res)=>{
         const { id } = req.params;
         const camp = await Campground.findById(id);
@@ -41,14 +46,16 @@ module.exports = {
         }
         res.render('campgrounds/edit', {camp});
     },
-    
+
+    // Update edited campground route
     updateCamp: async(req,res)=>{
         const {id} = req.params;
         const updatedCamp = await Campground.findByIdAndUpdate(id,{...req.body.campground},{new:true});
         req.flash('success',`Successfully updated ${updatedCamp.title}!`);
         res.redirect(`/campgrounds/${updatedCamp._id}`);
     },
-    
+
+    // delete campground route
     deleteCamp: async(req,res)=>{
         const {id} = req.params;
         await Campground.findByIdAndDelete(id);
