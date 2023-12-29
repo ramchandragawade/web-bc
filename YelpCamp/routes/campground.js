75c1daf834ff43efd08c465310c193d24ee7dae8
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 16:14:02 +0530

    temp changes

--- YelpCamp/routes/campground.js
+++ YelpCamp/routes/campground.js
@@ -4,12 +4,18 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgroundController = require('../controllers/campground');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 // (get) Campgroung index route
 // (post) Add New campground form submission route(POST)
 router.route('/')
 .get(campgroundController.index)
-.post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCamp));
+// .post(isLoggedIn, validateCampground, catchAsync(campgroundController.createCamp));
+.post(upload.array('image'),(req,res)=>{
+    console.log(req.body,req.files);
+    res.send('done')
+});
 
 // New campground form route
 router.get('/new', isLoggedIn, campgroundController.renderNewForm);
