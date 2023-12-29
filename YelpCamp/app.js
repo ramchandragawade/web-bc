50557ce4af6afa171626334995b7af372753506d
ramchandragawade <raajgawade39@gmail.com>
2023-12-29 16:21:34 +0530

    temp changes 2

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV!='production'){
+    require('dotenv').config();
+}
+console.log(process.env.NODE_ENV,process.env.SECRET);
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
