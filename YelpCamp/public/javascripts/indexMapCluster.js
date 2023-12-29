4e52691e5a78e6c880e49c3b06129b002654f2cc
ramchandragawade <raajgawade39@gmail.com>
2023-12-30 04:22:56 +0530

    using geodata virtual to show points

--- YelpCamp/public/javascripts/indexMapCluster.js
+++ YelpCamp/public/javascripts/indexMapCluster.js
@@ -113,10 +113,7 @@ map.on('load', () => {
     // description HTML from its properties.
     map.on('click', 'unclustered-point', (e) => {
         const coordinates = e.features[0].geometry.coordinates.slice();
-        console.log(e.features[0]);
-        const mag = e.features[0].properties.mag;
-        const tsunami =
-            e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+        const props = e.features[0].properties;
 
         // Ensure that if the map is zoomed out such that
         // multiple copies of the feature are visible, the
@@ -128,7 +125,7 @@ map.on('load', () => {
         new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(
-                `magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
+                `${props.popUpMark}`
             )
             .addTo(map);
     });
