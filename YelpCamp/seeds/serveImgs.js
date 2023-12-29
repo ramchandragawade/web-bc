6536aed275b1a9bcc052947d70269b9a7d501dd3
ramchandragawade <raajgawade39@gmail.com>
2023-12-30 01:01:06 +0530

    seeds helper

---
+++ YelpCamp/seeds/serveImgs.js
@@ -0,0 +1,42 @@
+module.exports.sampleImgs = [
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850388/YelpCamp/pztqgnrfmclzubara1qd.jpg',
+        filename: 'YelpCamp/pztqgnrfmclzubara1qd'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850390/YelpCamp/x1z2iviujod6xm8xnqft.jpg',
+        filename: 'YelpCamp/x1z2iviujod6xm8xnqft'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850392/YelpCamp/xfosz7uc8uimdykwapoe.jpg',
+        filename: 'YelpCamp/xfosz7uc8uimdykwapoe'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850393/YelpCamp/o8xe0fgp7phbzrk7epu1.jpg',
+        filename: 'YelpCamp/o8xe0fgp7phbzrk7epu1'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850395/YelpCamp/bkiyu4yzvl3bnnbxqvnp.jpg',
+        filename: 'YelpCamp/bkiyu4yzvl3bnnbxqvnp'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850396/YelpCamp/bvufzgq0kicckzml47w0.jpg',
+        filename: 'YelpCamp/bvufzgq0kicckzml47w0'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850397/YelpCamp/ocwtlmajygkjqiult1bj.jpg',
+        filename: 'YelpCamp/ocwtlmajygkjqiult1bj'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850399/YelpCamp/uglkicz7nae3jqz1jpfo.jpg',
+        filename: 'YelpCamp/uglkicz7nae3jqz1jpfo'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850400/YelpCamp/g1lrh1splmmbr6jvjlen.jpg',
+        filename: 'YelpCamp/g1lrh1splmmbr6jvjlen'
+    },
+    {
+        url: 'https://res.cloudinary.com/duhlvrita/image/upload/v1703850402/YelpCamp/f5tfaxh5hnkgyvntcwqz.jpg',
+        filename: 'YelpCamp/f5tfaxh5hnkgyvntcwqz'
+    }
+];
