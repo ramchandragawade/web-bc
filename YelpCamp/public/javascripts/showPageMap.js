43f72aed46065e8ed9e0e108386053826c670a59
ramchandragawade <raajgawade39@gmail.com>
2023-12-30 02:39:13 +0530

    changed map style and added a pop up on marker

--- YelpCamp/public/javascripts/showPageMap.js
+++ YelpCamp/public/javascripts/showPageMap.js
@@ -3,11 +3,14 @@ const campGeo = JSON.parse(campGeometry)
 const coords = campGeo && campGeo.coordinates;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/light-v11', // style URL
     center: coords, // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 
-const marker1 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+new mapboxgl.Marker({ color: 'black', rotation: 45 })
 .setLngLat(coords)
+.setPopup(new mapboxgl.Popup({offset:25})
+.setHTML(`<h6 class="mt-2 mb-1 me-3 lh-1">${campTitle}</h6>
+<p class="mb-1 mt-1 text-muted">${campLocation}</p>`))
 .addTo(map);
