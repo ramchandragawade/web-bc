3a2c6e2fbc14470e0abda353f6f9dc05cf9e1b3e
ramchandragawade <raajgawade39@gmail.com>
2023-12-30 20:57:07 +0530

    error sort

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -32,16 +32,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
-app.use(mongoSanitize());
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
 const session = require('express-session');
 const flash = require('connect-flash');
 const sessionCfg = {
-    secret: 'CAMPGROUNDSECRET123',
+    name: 'sesh',
+    secret: process.env.SESSION_CFG_SECRET,
     resave:false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure:true,
         maxAge: 1000*60*60*24*7
     }
 }
@@ -55,7 +59,6 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
-    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
