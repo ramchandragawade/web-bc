a1c4fc58cc38bf40572d5240d53915dad8c75719
ramchandragawade <raajgawade39@gmail.com>
2023-12-30 21:16:40 +0530

    removed log

--- YelpCamp/validationSchemas.js
+++ YelpCamp/validationSchemas.js
@@ -14,7 +14,6 @@ const extension = (joi) => ({
                   allowedTags: [],
                   allowedAttributes: {},
                 });
-                console.log('isclean:', clean);
                 if (clean) {
                     return clean;
                 }
